12b66319ca9a75c4f6f2e42788f1151cbe0521f1
muhammadbabar3457-droid <muhammadbabar3457@gmail.com>
2026-09-15 10:07:05 +0500

    Add MainActivity in correct application namespace

---
+++ app/src/main/java/com/aimantraders/ammart/MainActivity.java
@@ -0,0 +1,29 @@
+package com.aimantraders.ammart;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        webView = new WebView(this);
+        setContentView(webView);
+        WebSettings s = webView.getSettings();
+        s.setJavaScriptEnabled(true);
+        s.setDomStorageEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://muhammadbabar3457-droid.github.io/AM-MART/");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView != null && webView.canGoBack()) webView.goBack();
+        else super.onBackPressed();
+    }
+}
